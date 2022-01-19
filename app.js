abf4304c72eb9e49eee7b7bd74eda56d16474428
Lukas <rasocha.lukas@gmail.com>
2022-01-19 13:22:00 +0100

    addded routes for getting a specific id

--- app.js
+++ app.js
@@ -22,7 +22,17 @@ const server = http.createServer(async (req,res) => {
             res.writeHead(200, {"Content-Type": "application/json"});
             res.end(JSON.stringify(todos));
             break;
+        case(req.url.match(/\api\/todos\/([0-9]+)/) && req.method === "GET"):
+            try {
+            const id = req.url.split("/")[3];
+            const todo = await new Todo().getTodo(id);
+            res.writeHead(200, {"Content-type": "application/json"});
 
+            res.end(JSON.stringify(todo));
+            } catch (error) {
+            res.writeHead(404, {"Content-Type": "application/json"});
+            res.end(JSON.stringify({message:error}));
+            }
 
     }
 
--- controller.js
+++ controller.js
@@ -6,6 +6,17 @@ class Controller {
     async listTodos() {
         return new Promise((resolve,_) => resolve(data));
     }
+
+    async getTodo(id) {
+        return new Promise((resolve,reject) => {
+            let todo = data.find((todo) => todo.id === parseInt(id));
+            if(todo) {
+                resolve(todo);
+            } else{
+                reject(`Todo with id ${id} not found`);
+            }
+        });
+    }
 };
 
 module.exports = Controller;
